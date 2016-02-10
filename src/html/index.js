dcb4f5fa25269dd39738a755ec325b82a01edd85
Denis Sokolov <denis.sokolov@dashlane.com>
2016-02-10 07:04:55 +0200

    Base64 encode the data URL

--- src/html/index.js
+++ src/html/index.js
@@ -13,6 +13,8 @@ const byContent = urlString => {
     return '<video controls autoplay src="' + escape(url) + '"></video>'
 }
 
-module.exports = url => 'data:text/html,' +
-    fs.readFileSync(__dirname + '/main.html', 'utf-8') +
-    byContent(url)
+module.exports = url => 'data:text/html;base64,' +
+    new Buffer(
+        fs.readFileSync(__dirname + '/main.html', 'utf-8') +
+        byContent(url)
+    ).toString('base64')
